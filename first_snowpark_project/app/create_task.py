ab0bf2c6a39e1744fd196e9bd7e7a8da4d98c67b
pradeepetl <59853089+pradeepetl@users.noreply.github.com>
2024-07-27 15:04:24 +0000

    u

--- first_snowpark_project/app/create_task.py
+++ first_snowpark_project/app/create_task.py
@@ -39,22 +39,22 @@ tasks = root.databases["demo_db"].schemas['public'].tasks
 #tasks.create(my_task)
 
 # create dag  
-with DAG("my_dag",schedule=timedelta(days=1),use_func_return_value=True,stage_location="@dev_deployment",warehouse="compute_wh") as dag:
+with DAG("my_dag",schedule=timedelta(days=1),use_func_return_value=True,stage_location="@demo_db.public.dev_deployment",warehouse="compute_wh") as dag:
   dag_task_1 =  DAGTask("my_hello_task",StoredProcedureCall(procedures.hello_procedure,args=["pradeep"],\
     input_types=[StringType()],return_type=StringType(), packages=["snowflake-snowpark-python"],imports=["@dev_deployment/my_snowpark_project/app.zip"],\
-    stage_location="@dev_deployment"),warehouse="compute_wh")
+    stage_location="@demo_db.public.dev_deployment"),warehouse="compute_wh")
   
   dag_task_2 =  DAGTask("my_test_task",StoredProcedureCall(procedures.test_procedure,\
     packages=["snowflake-snowpark-python"],imports=["@dev_deployment/my_snowpark_project/app.zip"],\
-    stage_location="@dev_deployment"),warehouse="compute_wh")
+    stage_location="@demo_db.public.dev_deployment"),warehouse="compute_wh")
   
   dag_task_3 =  DAGTask("my_test_task3",StoredProcedureCall(procedures.test_procedure_two,\
     packages=["snowflake-snowpark-python"],imports=["@dev_deployment/my_snowpark_project/app.zip"],\
-    stage_location="@dev_deployment"),warehouse="compute_wh")
+    stage_location="@demo_db.public.dev_deployment"),warehouse="compute_wh")
   
   dag_task_4 =  DAGTask("my_test_task4",StoredProcedureCall(procedures.test_procedure,\
     packages=["snowflake-snowpark-python"],imports=["@dev_deployment/my_snowpark_project/app.zip"],\
-    stage_location="@dev_deployment"),warehouse="compute_wh")
+    stage_location="@demo_db.public.dev_deployment"),warehouse="compute_wh")
   
   dag_task_1 >> dag_task_2 >> [dag_task_3,dag_task_4]
   
