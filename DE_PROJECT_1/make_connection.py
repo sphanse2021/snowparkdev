37fffd8bccd376cb4edeab37c0dd933d54d95f5b
sphanse2021 <87150932+sphanse2021@users.noreply.github.com>
2024-10-14 20:19:17 +0000

    Added file in DE_Project Folder

--- DE_PROJECT_1/make_connection.py
+++ DE_PROJECT_1/make_connection.py
@@ -9,4 +9,4 @@ directory_path= sys.argv[1]
 os.chdir(f"{directory_path}")
 # Make sure all 6 SNOWFLAKE_ environment variables are set
 # SnowCLI accesses the passowrd directly from the SNOWFLAKE_PASSWORD environmnet variable
-os.system(f"snow connection add --account $SNOWFLAKE_ACCOUNT --user $SNOWFLAKE_USER --warehouse $SNOWFLAKE_WAREHOUSE --database $SNOWFLAKE_DATABASE --schema $SNOWFLAKE_SCHEMA --role $SNOWFLAKE_ROLE --connection-name default")  
+os.system(f"snow connection add --account $SNOWFLAKE_ACCOUNT --user $SNOWFLAKE_USER --warehouse $SNOWFLAKE_WAREHOUSE --database $SNOWFLAKE_DATABASE --schema $SNOWFLAKE_SCHEMA --role $SNOWFLAKE_ROLE --connection-name default")   
